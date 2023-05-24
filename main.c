89fd01eca93b0e83f9adcb854414b3dea6dba0a7
Giovanna de Souza <giovanna.silva758@etec.sp.gov.br>
2023-05-24 18:04:43 -0300

    adicionei o projeto de hoje

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+	
+int numero1;
+int numero2;
+
+numero1=10;
+numero2=20;
+
+printf("digite um valor para o numero 1: ");
+scanf("%d" ,&numero1);	
+printf("o numero 1:%d/no numero 2:%d",numero1,numero2);
+
+
+}
